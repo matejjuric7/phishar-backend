95bcacd714b5bbf638e9442f776b1b00f15e5944
Matej Juric <matej.juric@speck.agency>
2021-09-16 23:20:30 +0200

    Add url validation to backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,13 @@ const router = express.Router();
 router.get('/', (_, res) => res.json({ ok: 'ok' }));
 
 router.post('/', async (req, res) => {
+  const urlRegex =
+    /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
   try {
+    const { url } = req.body;
+    if (!urlRegex.test(url)) {
+      throw new Error('Not a valid url');
+    }
     const data = await linkScraper(req.body.url);
     if (!data) {
       throw new Error('Something went wrong');
